d6779c4569ec03cb85248bc3a3f7e2fb026835e5
cakkocak <14230003@nusamandiri.ac.id>
2026-09-24 13:11:56 +0700

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -14,15 +14,6 @@ function App() {
         <Route path="/host/:quizId" element={<HostViewWrapper />} />
         <Route path="/join" element={<PlayerView />} />
       </Routes>
-      <!-- Google tag (gtag.js) -->
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-WXJGB5P4QK"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-WXJGB5P4QK');
-      </script>
     </BrowserRouter>
   );
 }
